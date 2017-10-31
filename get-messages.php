e40fa956541d16515a2d1369e1b3b9739b4806cd
BaffourAdu <baffouraduboampong@gmail.com>
2017-10-31 07:09:32 +0000

    Updated the UI and added Functionality
    The UI has been updated, the index and questions html has been put together and added Javascript as well as php o handle database submission and retrival.

---
+++ get-messages.php
@@ -0,0 +1,18 @@
+<?php
+$db = new mysqli("localhost", "msg", "", "");
+
+if ($db->connect_error) {
+	die("Sorry, there was a problem connecting to our database.");
+}
+
+$username = stripslashes(htmlspecialchars($_GET['username']));
+$result = $db->prepare("SELECT * FROM messages");
+$result->execute();
+$result = $result->get_result();
+
+while ($r = $result->fetch_row()) {
+	echo $r[1];
+	echo "\\";
+	echo $r[2];
+	echo "\n";
+}
